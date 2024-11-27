99ed43fa446964c0af5618ab5b906b28ed4d9c01
Novruzoff <95917545+Novruzoff@users.noreply.github.com>
2024-11-26 23:58:14 -0500

    Initial Commit

---
+++ Intibah/IntibahApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntibahApp.swift
+//  Intibah
+//
+//  Created by Murad Novruzov on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct IntibahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IntibahTests/IntibahTests.swift
@@ -0,0 +1,17 @@
+//
+//  IntibahTests.swift
+//  IntibahTests
+//
+//  Created by Murad Novruzov on 2024-11-26.
+//
+
+import Testing
+@testable import Intibah
+
+struct IntibahTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
